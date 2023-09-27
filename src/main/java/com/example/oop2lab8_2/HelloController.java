14e7d119620f88f67c5b0850a8b9e870a5311f85
Willuku <le1952006@crc-lennox.qc.ca>
2023-09-27 09:07:18 -0400

    Created williamButton which displays a message

--- src/main/java/com/example/oop2lab8_2/HelloController.java
+++ src/main/java/com/example/oop2lab8_2/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.oop2lab8_2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -10,5 +11,9 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void onWilliamButtonClick(ActionEvent actionEvent) {
+        welcomeText.setText("This button was developed by William Clark.");
     }
 }
